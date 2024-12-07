7afcf2bb03e0bf563fc5e36caa8271dd4a9763d1
unknown <wdm46857@gmail.com>
2024-12-07 18:22:51 +0200

    update

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -16,8 +17,8 @@ public class Main {
             List<Process> processList = new ArrayList<>();
             for (int i = 1; i <= n; i++) {
                 System.out.println("Enter details for Process " + i + ":");
-                System.out.println("Enter Process Name");
-                String ProcessName = scanner.next() ;                
+                System.out.print("Enter Process Name: ");
+                String ProcessName = scanner.next() ;
                 System.out.print("Arrival Time: ");
                 int arrivalTime = scanner.nextInt();
                 System.out.print("Burst Time: ");
@@ -33,9 +34,42 @@ public class Main {
 
 
             CPUScheduler cpuScheduler = new CPUScheduler() ;
-            cpuScheduler.SJF(processList);
-           cpuScheduler.SRTFScheduler(processList, contextSwitchTime);
-            cpuScheduler.FCAIScheduler(processList, contextSwitchTime);
+
+            boolean running = true;
+            while (running) {
+                System.out.println("\nSelect Scheduling Algorithm:");
+                System.out.println("1. Shortest Job First (SJF)");
+                System.out.println("2. Priority Scheduling");
+                System.out.println("3. Shortest Remaining Time First (SRTF)");
+                System.out.println("4. First Come, Arrival Impact (FCAI)");
+                System.out.println("5. Exit");
+                System.out.print("Enter your choice: ");
+                int choice = scanner.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        cpuScheduler.SJF(processList);
+                        break;
+                    case 2:
+                        cpuScheduler.priorityScheduler(processList, contextSwitchTime);
+                        break;
+                    case 3:
+                        cpuScheduler.SRTFScheduler(processList, contextSwitchTime);
+                        break;
+                    case 4:
+                        cpuScheduler.FCAIScheduler(processList, contextSwitchTime);
+                        break;
+                    case 5:
+                        running = false;
+                        System.out.println("Exiting the program.");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                        break;
+                }
+            }
         }
+
+
     }
 }
